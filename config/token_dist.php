87b9d1fad0464caf3c76350470dfcc5e7b67362d
m4rthiz <m4rthiz@gmail.com>
2017-10-13 01:24:13 +0200

    add token_dist

---
+++ config/token_dist.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mar
+ * Date: 13/10/17
+ * Time: 00:25
+ */
+
+
+/**
+ * Put here your github token
+ * @var string $token
+ */
+$token = '';
+
+?>
--- curl.php
+++ curl.php
@@ -2,8 +2,9 @@
 
 require_once 'CurlConnect.php';
 
+
 $connect = new \Wcs\CurlConnect();
 
-print_r($connect->getConnect('Aurelie-feyzin'));
+print_r($connect->getConnect('m4rthiz', $token));
 
 //print_r($connect);
