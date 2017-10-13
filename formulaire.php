52c2c45eafa97117f816d7cad5550b72f301e1d6
m4rthiz <m4rthiz@gmail.com>
2017-10-13 06:49:48 +0200

    add html in form

--- formulaire.php
+++ formulaire.php
@@ -12,8 +12,9 @@ if (isset($_POST)) {
     $connect = new \Wcs\CurlConnect();
     if (isset($_POST['user']) and !empty($_POST['user'])) {
         $user = $_POST['user'];
+        $link = $connect->getLink($user);
         $result = $connect->getConnect($user,  $token);
-        print_r($result);
+
 
     }
     if (isset($_POST['avatar']) and !empty($_POST['avatar'])) {
@@ -62,7 +63,7 @@ if (isset($_POST)) {
         <div class="col-xs-4 col-sm-4">
         <form role="form" name="form" action="" method="post">
             <div class="form-group ">
-                <label for="user">Github User</label>
+                <label for="user"><?php echo $link?></label>
                 <input type="text" name="user" class="form-control" id="user"
                        placeholder="User" required>
             </div>
@@ -91,7 +92,34 @@ if (isset($_POST)) {
         </form>
             <div class="form-group">
                 <label for="comment">Code</label>
-                <textarea class="form-control" rows="5" id="comment"></textarea>
+                <textarea class="form-control" rows="5" id="comment">
+                    <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <div class="container bg-info col-xs-2" style="padding: 5px;border-radius: 5px">
+        <div class="media ">
+            <div class="media-left">
+                <a href="#">
+                    <img class="media-object" style="width: 70px" src="https://avatars1.githubusercontent.com/u/25553263?v=4" alt="avatar">
+                </a>
+            </div>
+            <div class="media-body">
+                <a href="https://github.com/m4rthiz" class="media-heading ">github.com/m4rthiz</a>
+                <p>repositories <span class="badge">42</span></p>
+                <p>followers <span class="badge">42</span></p>
+            </div>
+            <p class="btn btn-default btn-xs btn-block" href="#" role="button">mon repo 1</p>
+            <p class="btn btn-default btn-xs btn-block" href="#" role="button">mon repo 2</p>
+            <p class="btn btn-default btn-xs btn-block" href="#" role="button">mon repo 3</p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+                </textarea>
             </div>
         </div>
 
