98cfcdac5073014dc9faa71604540b7f99012107
m4rthiz <m4rthiz@gmail.com>
2017-10-13 11:35:23 +0200

    change style card

--- textarea.php
+++ textarea.php
@@ -9,7 +9,7 @@
         </div>
         <div class="media-body">
             <a href="<?php echo $link ?>" class="media-heading "><?php echo $user ?></a>
-            <?php echo !empty($_POST['nbRepos']) ? '<p>Repositories <span class="badge">' . $nbRepo . '</span></p>' : '' ?>
+            <?php echo !empty($_POST['nbRepos']) ? '<p>Repos <span class="badge">' . $nbRepo . '</span></p>' : '' ?>
             <?php echo !empty($_POST['nbFollowers']) ? '<p>Followers <span class="badge">' . $nbFollowers . '</span></p>' : '' ?>
         </div>
         <?php foreach ($threeRepos as $repo): ?>
